e474d43f1c73df0128e810d90fba8688174da2c8
MrElektronix <milovanpelt@gmail.com>
2017-01-09 16:11:06 +0100

    walls

---
+++ Assets/Scripts/WallDistance.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class WallDistance : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject wall;
+
+    private Vector3 offset;
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - wall.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = wall.transform.position + offset;
+    }
+}
