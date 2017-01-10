46b11684d3cb153339dd22414d280e0677032f0d
casperheringa <5be8141b@opayq.com>
2017-01-10 09:35:22 +0100

    update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     private float speed = 2f;
     private Transform pos;
+    
 
     // Use this for initialization
     void Start()
@@ -16,11 +16,19 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
         //transform.Rotate(0, x, 0);
         transform.Translate(x, 0, z);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 4;
+        }
+        else
+        {
+            speed = 2;
+        }
     }
 }
 	
