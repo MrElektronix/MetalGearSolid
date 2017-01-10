97d74faea7ffd0d617a030994a4f7a4ef03bf1c9
MrElektronix <milovanpelt@gmail.com>
2017-01-10 09:41:36 +0100

    update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,31 +5,24 @@ public class PlayerMovement : MonoBehaviour
 {
     private float speed = 2f;
     private Transform pos;
-    
 
     // Use this for initialization
     void Start()
     {
-   
+
     }
 
 
     void Update()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
         //transform.Rotate(0, x, 0);
         transform.Translate(x, 0, z);
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            speed = 4;
-        }
-        else
-        {
-            speed = 2;
-        }
     }
+}
+	
 }
 	
 	
