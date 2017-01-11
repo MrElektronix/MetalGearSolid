31eaf41abb11e7f1833ec6dc947ca51df1a2f9c5
MrElektronix <milovanpelt@gmail.com>
2017-01-11 09:42:52 +0100

    update

---
+++ Assets/Scripts/CheckTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckTrigger : MonoBehaviour {
+    public static bool nearWall;
+
+    void Start()
+    {
+        nearWall = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("walltrigger"))
+        {
+            nearWall = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("walltrigger"))
+        {
+            nearWall = false;
+        }
+    }
+
+
+}
