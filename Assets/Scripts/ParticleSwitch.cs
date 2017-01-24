1a469badf857ba78627bbad1d5825a9fe57813d5
Justin van Gemen <20277@ma-web.nl>
2017-01-24 14:40:18 +0100

    Personal Update (Scene) + Art Import

---
+++ Assets/Scripts/ParticleSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSwitch : MonoBehaviour {
+
+    private ParticleSystem _pSystem;
+
+	void Start ()
+    {
+        _pSystem = GetComponent<ParticleSystem>();
+        StartCoroutine(ParticleSystemSwitchOnLoad());
+    }
+
+    void Update()
+    {
+        var _psMain = _pSystem.main;
+        if (Input.GetKey(KeyCode.M))
+        {
+            if (!_pSystem.isPlaying)
+            {
+                _pSystem.Play();
+                Debug.Log("no-rip");
+            }
+            if (_pSystem.isPlaying)
+            {
+                Debug.Log("rip");
+                _pSystem.Stop();
+            }
+        }
+    }
+
+    IEnumerator ParticleSystemSwitchOnLoad()
+    {
+        yield return new WaitForSeconds(1);
+        if (_pSystem.isPlaying)
+        {
+            Debug.Log("autorip");
+            _pSystem.Stop();
+        }
+    }
+
+}
