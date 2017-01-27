8f5984ae5e7de9816a38f77ac403885689fef534
Justin van Gemen <20277@ma-web.nl>
2017-01-27 09:34:13 +0100

    Player walking sound fix!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,16 +9,32 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Rigidbody _rigidbody;
 
+    [SerializeField]
+    private AudioSource _audio0;
+    [SerializeField]
+    private AudioClip _walking;
+    private bool _audioPlaying;
 
     // Use this for initialization
     private void Start()
     {
+         _audioPlaying = _audio0.isPlaying;
         _rigidbody = GetComponent<Rigidbody>();
     }
 
 
     private void Update()
     {
+        _audioPlaying = _audio0.isPlaying;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            if (_audioPlaying == false)
+            {
+                _audio0.PlayOneShot(_walking, 0.1F);
+                Debug.Log("isPlaying");
+            }
+        }
+
         var x = Input.GetAxisRaw("Horizontal") * Speed;
         var z = Input.GetAxisRaw("Vertical") * Speed;
         _rigidbody.velocity = new Vector3(x, 0f, z);
